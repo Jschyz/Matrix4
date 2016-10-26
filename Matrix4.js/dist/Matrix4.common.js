e57b604664a64d48579044f4521bda52c7daf572
huihui <huihui@huihuis-MacBook-Pro.local>
2016-10-26 14:14:01 +0800

    rollup.js build framework

---
+++ Matrix4.js/dist/Matrix4.common.js
@@ -0,0 +1,10 @@
+/*!
+ * Matrix4 v0.1.0
+ * (c) 2016 skyfei
+ * Released under the MIT License.
+ */
+'use strict';
+
+function Matrix4() {}
+
+module.exports = Matrix4;
---
+++ Matrix4.js/dist/Matrix4.js
@@ -0,0 +1,16 @@
+/*!
+ * Matrix4 v0.1.0
+ * (c) 2016 skyfei
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Matrix4 = factory());
+}(this, (function () { 'use strict';
+
+function Matrix4() {}
+
+return Matrix4;
+
+})));
